760ceec91e8e24cb62cf5384f94b75a2dae97051
Vivian Wambui <124568290+vivianwambui@users.noreply.github.com>
2024-01-26 08:38:09 +0300

    Add files via upload

---
+++ Bridal Wedding Planner.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bridal Wedding Planner</title>
+<BODY Bgcolor ="PINK" Text = "Black">
+<center><H1>Bridal Wedding Planner </H1></center>
+<H2 align = "center"><I>Welcome</I></H2>
+
+<HR color = "magenta" size = "5"><Marquee behavior="alternate">Welcome</Marquee><p><B><H1>Welcome<p></B></H1>
+<img src="C:\Users\hp\AppData\Local\Temp\Rar$DIa0.496\wedding_rings_515226.jpg" width="1000" height="600" title="y image" alt="wedding" border="5">
+</head>
+<body>
+  <header>
+    <h1>Wedding Planner</h1>
+  </header>
+
+  <main>
+    <section>
+      <h2>About Us</h2>
+      <p>Welcome to Bridal Wedding Planner, where dreams come true! We are a dedicated team of experienced wedding planners, passionate about creating unforgettable moments for couples on their special day.</p>
+      <p>Our goal is to bring your vision to life, reflecting your unique style and personality in every element. Whether you desire an intimate garden wedding or a grand luxurious affair, we tailor our services to your specific needs, making your dream wedding a reality.</p>
+  <p>Contact us today to schedule a consultation and let us begin planning the wedding of your dreams.</p>
+ 
+    </section>
+
+    <section>
+      <h2>Services</h2>
+      <ul>
+        <li>Venue Selection whereby we assist in finding the perfect wedding venue based on the couple's preferences, budget, and guest count.</li>
+        <li>Budget Management by helping couples establish and manage their wedding budget, providing guidance on cost-saving measures and allocating funds to various aspects of the wedding.</li>
+        <li>Event Design and Styling where we creat a cohesive theme and style for the wedding, including décor, color schemes, and overall aesthetic.</li>
+        <li>Timeline and Logistics by developing a detailed timeline for the wedding day and coordinating logistics such as transportation, accommodation, and guest arrangements.</li>
+        <li>Ceremony and Reception Planning through helping with ceremony and reception layout, seating arrangements, and coordinating the order of events.</li>
+        
+      </ul>
+    </section>
+
+    <section>
+      <h2>Contact</h2>
+      <p>+254-998-9876</p>
+      <p>+254-456-0988</p>
+      <p>kevin@gmail.com</p>
+      <p>vivian1@gmail.com</p>
+    </section>
+
+    <section>
+    	<h2>Location</h2>
+    	<p>Nairobi, Kenya-0001-10</p>
+    </section>
+  </main>
+
+  <footer>
+    <p>© 2023 Bridal Wedding Planner. All rights reserved.</p>
+  </footer>
+</body>
+</html 254-998-9876
